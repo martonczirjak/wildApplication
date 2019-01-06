281545f8034060b0fbbe4daa5e6d1682a3a61cc6
martonczirjakhome <marton.czirjak@mmsone.com>
2019-01-06 16:38:35 +0100

    Last commit unstaged files

--- app/src/main/java/com/plixapp/czirjak/wildjavaapplication/login/LoginActivity.java
+++ app/src/main/java/com/plixapp/czirjak/wildjavaapplication/login/LoginActivity.java
@@ -1,15 +1,22 @@
 package com.plixapp.czirjak.wildjavaapplication.login;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.plixapp.czirjak.wildjavaapplication.R;
-
+import com.plixapp.czirjak.wildjavaapplication.databinding.ActivityLoginBinding;
+/**
+ * A login screen that offers login via email/password.
+ */
 public class LoginActivity extends AppCompatActivity {
 
+    private ActivityLoginBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_login);
+
+
     }
 }
--- app/src/main/java/com/plixapp/czirjak/wildjavaapplication/wilds/WildsActivity.java
+++ app/src/main/java/com/plixapp/czirjak/wildjavaapplication/wilds/WildsActivity.java
@@ -1,19 +1,18 @@
-package com.plixapp.czirjak.wildjavaapplication.login;
+package com.plixapp.czirjak.wildjavaapplication.wilds;
 
 import android.databinding.DataBindingUtil;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.plixapp.czirjak.wildjavaapplication.R;
-import com.plixapp.czirjak.wildjavaapplication.databinding.ActivityLoginBinding;
+import com.plixapp.czirjak.wildjavaapplication.databinding.ActivityWildsBinding;
 
-public class LoginActivity extends AppCompatActivity {
+public class WildsActivity extends AppCompatActivity {
 
-    private ActivityLoginBinding binding;
+    private ActivityWildsBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_login);
-
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_wilds);
     }
 }
